d470fd7c6f819c2ae887a05be85a0207c1bfae1e
Milan <Milan@172.23.184.227>
2014-09-07 15:08:38 +0800

    Folder structure

---
+++ src/java/sg/edu/nus/iss/phoenix/maintainSchedule/dao/WeeklyScheduleDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nus.iss.phoenix.maintainSchedule.dao;
+
+/**
+ *
+ * @author Milan
+ */
+public interface WeeklyScheduleDao {
+    
+}
