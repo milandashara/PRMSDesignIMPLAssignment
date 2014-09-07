a7e486f884fa182c82d2e35252072133347907d3
Milan <Milan@172.23.184.227>
2014-09-07 15:04:19 +0800

    Folder structure

---
+++ src/java/sg/edu/nus/iss/phoenix/maintainSchedule/dao/impl/AnnualScheduleDaoImpl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sg.edu.nus.iss.phoenix.maintainSchedule.dao.impl;
+
+import sg.edu.nus.iss.phoenix.maintainSchedule.dao.AnnualScheduleDao;
+
+/**
+ *
+ * @author Milan
+ */
+public class AnnualScheduleDaoImpl implements AnnualScheduleDao{
+    
+}
